8d3d62aaa1a16f1d901c616848b982772c90ae0a
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-08-06 18:40:39 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+const getUpdateTime = (timeUpdate) => {
+    const currentTime = timeUpdate.seconds;
+
+    localStorage.setItem('videoplayer-current-time', JSON.stringify({ currentTime }));
+};
+
+const getTrottleUpdateTime = throttle(getUpdateTime, 1000)
+player.on('play', getTrottleUpdateTime);
+
+const savedTime = localStorage.getItem('videoplayer-current-time')
+const parsedTime = JSON.parse(savedTime);
+
+
+player.setCurrentTime(parsedTime.currentTime).then(function (seconds) {
+    
+});
 
